2b249e9ea287ccdf7b8f05ab40735265f665cd64
parallelpandas <parallelpandas@gmail.com>
2018-07-04 14:47:43 +0000

    add correct_user check

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -10,11 +11,11 @@ class TasksController < ApplicationController
   end
   
   def new
-    @task=Task.new()
+    @task=current_user.tasks.build()
   end
   
   def create
-    @task=Task.new(task_params)
+    @task=current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success]='Taskが正常に投稿されました'
@@ -55,8 +56,14 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    return params.require(:task).permit(:content, :status, :user_id)
+    return params.require(:task).permit(:content, :status)
   end
-  
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to tasks_url()
+    end
+  end  
   
 end
